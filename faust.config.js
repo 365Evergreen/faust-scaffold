0f346eee7ae61b516a1b6dcc3763888e74336e51
365Evergreen <55726264+365Evergreen@users.noreply.github.com>
2026-01-06 14:19:52 +0000

    feat(config): remove deprecated `canonizeResults` option from Apollo cache options

--- faust.config.js
+++ faust.config.js
@@ -1,6 +1,21 @@
 import { setConfig } from "@faustwp/core";
+import { addFilter } from "@wordpress/hooks";
 import templates from "./wp-templates";
 import possibleTypes from "./possibleTypes.json";
+
+// Remove deprecated `canonizeResults` option from Apollo cache options
+// so builds with newer @apollo/client versions do not warn/fail.
+addFilter(
+  "faust.apolloClientInMemoryCacheOptions",
+  "faust-scaffold/remove-canonizeResults",
+  (options) => {
+    if (options && Object.prototype.hasOwnProperty.call(options, "canonizeResults")) {
+      // eslint-disable-next-line no-param-reassign
+      delete options.canonizeResults;
+    }
+    return options;
+  }
+);
 
 /**
  * @type {import('@faustwp/core').FaustConfig}

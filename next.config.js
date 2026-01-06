4d9ff0176977aca39cdbf5bc5dcccd2170782c0b
365Evergreen <55726264+365Evergreen@users.noreply.github.com>
2026-01-06 14:14:08 +0000

    ci: use Next static export via next.config.js output: 'export' and remove next export step

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const { withFaust } = require("@faustwp/core");
  * @type {import('next').NextConfig}
  **/
 module.exports = withFaust({
+  output: 'export',
   images: {
     domains: ["faustexample.wpengine.com"],
+    unoptimized: true,
   },
   trailingSlash: true,
 });
